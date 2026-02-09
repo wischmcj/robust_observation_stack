b5f7e48de872819b0c139ded4a60240026ccb4af
Collin Wischmcj <cjwischmeyer@gmail.com>
2026-02-08 21:13:58 -0500

    adding environment file to show needed ansible config options. Updating start.sh with loop for starting docker containers.

--- ansible/roles/target/templates/start.sh
+++ ansible/roles/target/templates/start.sh
@@ -1,11 +1,21 @@
+#!/bin/bash 
 set -e
 
 docker kill $(docker ps -q --filter="name=watchtower") || true
 docker rm $(docker ps -qa --filter="name=watchtower") || true
 
-docker kill $(docker ps -q --filter="name=archiveteam") || true
-docker rm $(docker ps -qa --filter="name=archiveteam") || true
+# docker kill $(docker ps -q --filter="name=archiveteam") || true
+# docker rm $(docker ps -qa --filter="name=archiveteam") || true
 
 docker run --detach --name watchtower --restart=on-failure --volume /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower --label-enable --include-restarting --cleanup --interval 3600
 port=8001
-docker run -d --name archiveteam  --env-file=/tmp/env --publish 8001:8001 --restart=on-failure --label=com.centurylinklabs.watchtower.enable=true --log-driver json-file --log-opt max-size=50m atdr.meo.ws/archiveteam/usgovernment-grab --concurrent 1 penguaman
+# docker run -d --name archiveteam  --env-file=/tmp/env --publish 8001:8001 --restart=on-failure --label=com.centurylinklabs.watchtower.enable=true  json-f--log-driverile --log-opt max-size=50m atdr.meo.ws/archiveteam/usgovernment-grab --concurrent 1 penguaman
+   
+# start your project container 
+for i in `seq 1 {{ ports }}` 
+do
+  docker run -d --name archiveteam_$i --publish $port:8001 --label=com.centurylinklabs.watchtower.enable=true --log-driver json-file --log-opt max-size=50m --restart=on-failure  atdr.meo.ws/archiveteam/usgovernment-grab --concurrent 6 penguaman
+  echo "archiveteam $i container started";
+  ((port+=1))
+done
+exit 0 
